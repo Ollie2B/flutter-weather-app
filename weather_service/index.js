e54a58d90722c10fc2fddbdd4c8dc766013099f7
Oliver <oliverklein33@gmail.com>
2023-12-15 00:42:36 -0300

    Initial commit

---
+++ weather_service/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+  res.send('Welcome to weather service API')
+});
+
+app.get('/weather', async (req, res) => {
+  const { city } = req.query;
+
+  if (!city) {
+    return res.status(400).json({ error: 'City name is required' });
+  }
+
+  try {
+    // Api config
+    const apiKey = 'API Key';
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    const response = await axios.get(apiUrl);
+    const weatherData = response.data;
+
+    // Extract key weather details
+    const simplifiedResponse = {
+      temperature: weatherData.main.temp,
+      tempMin: weatherData.main.temp_min,
+      tempMax: weatherData.main.temp_max,
+      description: weatherData.weather[0].description,
+      iconCode: weatherData.weather[0].icon,
+    };
+
+    res.status(200).json(simplifiedResponse);
+  } catch (error) {
+    console.error('Error fetching weather data:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
